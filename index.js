d18e9c2c2fd72f6f170ad70784133245e1a4d3ae
mmayerhofer <mmayerhofer@behaviorquant.com>
2020-01-10 09:55:37 +0100

    added code for GitlabCIBuildInfoPlugin
    - by default the file 'version.json' will be written to the webpack output directory,
      and contains information taken from gitlab-ci environment variables which are available on the gitlab-ci runner

---
+++ index.js
@@ -0,0 +1,36 @@
+class GitlabCIBuildInfoPlugin {
+  constructor(options) {
+    this.options = options || {
+      versionFileName: 'version.json'
+    };
+  }
+  apply(compiler) {
+    compiler.hooks.emit.tapAsync('VersionPlugin Plugin', (compilation, callback) => {
+      const commitSHA = process.env.CI_COMMIT_SHA || 'not.available';
+      const branch = process.env.CI_COMMIT_REF_NAME || 'not.available';
+      const jobID = process.env.CI_JOB_ID || 'not.available';
+      const pipelineId = process.env.CI_PIPELINE_ID || 'not.available';
+
+      const json = JSON.stringify({
+        builtAt: new Date().toISOString(),
+        gitInfo: {
+          commitSHA,
+          branch,
+        },
+        ciInfo:{
+          jobID,
+          pipelineId
+        }
+      });
+
+      compilation.assets[this.options.versionFileName] = {
+        source: () => json,
+        size: () => json.length
+      };
+
+      callback();
+    });
+  }
+}
+
+module.exports = GitlabCIBuildInfoPlugin;
