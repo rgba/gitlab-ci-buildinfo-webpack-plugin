cce4fc5ce06e4c8e6ff17c8d106b08cc28b45e4b
mmayerhofer <mmayerhofer@behaviorquant.com>
2020-01-10 11:15:45 +0100

    - fixed ID / Id naming inconsistency - updated version to 1.0.4

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ class GitlabCIBuildInfoPlugin {
     compiler.hooks.emit.tapAsync('VersionPlugin Plugin', (compilation, callback) => {
       const commitSHA = process.env.CI_COMMIT_SHA || 'not.available';
       const branch = process.env.CI_COMMIT_REF_NAME || 'not.available';
-      const jobID = process.env.CI_JOB_ID || 'not.available';
+      const jobId = process.env.CI_JOB_ID || 'not.available';
       const pipelineId = process.env.CI_PIPELINE_ID || 'not.available';
 
       const json = JSON.stringify({
@@ -18,7 +18,7 @@ class GitlabCIBuildInfoPlugin {
           branch,
         },
         ciInfo:{
-          jobID,
+          jobId,
           pipelineId
         }
       });
